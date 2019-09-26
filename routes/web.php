54789734187822948a35eb8820393db4835769bc
zhouhang <254253325@qq.com>
2019-09-26 22:49:06 +0800

    用户显示页面

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,6 @@ Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 
 Route::get('signup','UsersController@create')->name('signup');
-
+//Route::resource('users','UsersController')->name('users.show');
+Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 
