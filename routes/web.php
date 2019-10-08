7f2039fc8f31536606fbcdc3975a453c5f7c9f4f
zhouhang <254253325@qq.com>
2019-10-08 16:46:37 +0800

    创建会话

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('signup','UsersController@create')->name('signup');
 Route::get('/users/{user}', 'UsersController@show')->name('users.show');
 Route::post('store','UsersController@store')->name('users.store');
 
+Route::get('login','SessionsController@create')->name('login');
+Route::post('login','SessionsController@store')->name('login');
+Route::delete('logout','SessionsController@destroy')->name('logout');
+
