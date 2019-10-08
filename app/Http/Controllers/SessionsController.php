72830d54ddc2ec1032fe86b20f284c0e48262a6c
zhouhang <254253325@qq.com>
2019-10-08 20:15:00 +0800

    登陆时记住我

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -19,7 +19,7 @@ class SessionsController extends Controller
             'password' => 'required'
         ]);
 
-        if(Auth::attempt($credentials)){
+        if(Auth::attempt($credentials,$request->has('remember'))){
             //登陆成功后的相关操作
             session()->flash('success','欢迎回来!');
             return redirect()->route('users.show',[Auth::user()]);
@@ -36,5 +36,6 @@ class SessionsController extends Controller
         Auth::logout();
         session()->flash('success','您已成功退出');
         return redirect('login');
+//        return redirect()->route('login');
     }
 }
