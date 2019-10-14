713cc5d4d68658c497991737e22b63f2f46ae155
zhouhang <254253325@qq.com>
2019-10-14 22:10:30 +0800

    管理员可删除用户

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -14,13 +14,18 @@ class UserPolicy
      *
      * @return void
      */
-    public function __construct()
-    {
-        //
-    }
+//    public function __construct()
+//    {
+//        //
+//    }
 
     public function update(User $currentUser,User $user)
     {
         return $currentUser->id === $user->id;
+    }
+
+    public function destroy(User $currentUser, User $user)
+    {
+        return $currentUser->is_admin && $currentUser->id !== $user->id && $user->is_admin !==1;
     }
 }
